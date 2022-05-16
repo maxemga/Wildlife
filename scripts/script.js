458e211203c2c160b4382c6abed3215a301be289
Maxim <maxemga22@gmail.com>
2022-05-16 12:50:44 +0400

    Added burger menu

--- scripts/script.js
+++ scripts/script.js
@@ -3,17 +3,33 @@ document.addEventListener("click", (is_close) => {
 
     if (is_close.target.className == "donate") {
 
-        document.querySelector('.overlay').classList.add('active');
-        document.querySelector('.modal').classList.add('active');
+      document.querySelector('.overlay').classList.add('active');
+      document.querySelector('.modal').classList.add('active');
 
     }
 
     if (is_close.target.className == "modal__form__button") {
 
-        document.querySelector('.overlay').classList.remove('active');
-        document.querySelector('.modal').classList.remove('active');
+      document.querySelector('.overlay').classList.remove('active');
+      document.querySelector('.modal').classList.remove('active');
 
     }
+    if (is_close.target.className == "header__burger") {
+
+      document.querySelector('.modal__nav').classList.add('active');
+      document.querySelector('.overlay').classList.add('active');
+    }
+    if (is_close.target.className == "modal__nav__close_line") {
+
+      document.querySelector('.modal__nav').classList.remove('active');
+      document.querySelector('.overlay').classList.remove('active');
+    }
+    if (is_close.target.className == "modal__nav__ref") {
+
+      document.querySelector('.modal__nav').classList.remove('active');
+      document.querySelector('.overlay').classList.remove('active');
+    }
+
 
 });
 
@@ -32,15 +48,23 @@ for (let anchor of anchors) {
   })
 }
 
+let offset = 0;
+const sliderLine = document.querySelector('.articles__bottom');
 
-window.addEventListener('scroll', () => {
-  var currentScrollPos = window.pageYOffset;
+// document.querySelector('').addEventListener('click', function(){
+//     offset = offset + 256;
+//     if (offset > 768) {
+//         offset = 0;
+//     }
+//     sliderLine.style.left = -offset + 'px';
+// });
 
-  if(currentScrollPos > 1000) {
-    document.querySelector('.refund').classList.add('active');
-  }
+document.querySelector('.row').addEventListener('click', function () {
+  console.log('fsaf')
+    offset = offset - 256;
+    if (offset < 0) {
+        offset = 768;
+    }
+    sliderLine.style.left = -offset + 'px';
+});
 
-  else {
-    document.querySelector('.refund').classList.remove('active');
-  }
-})
